fd71abaead15bab76c79aaf3372c3df8e947a2c5
A Yahya Hudan Permana <yahyahudan19@gmail.com>
2021-02-16 05:58:27 +0700

    new db

--- db/mentoring20.sql
+++ db/mentoring20.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2021 at 06:41 AM
+-- Generation Time: Feb 15, 2021 at 11:57 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -33,6 +33,14 @@ CREATE TABLE `angkatan` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `angkatan`
+--
+
+INSERT INTO `angkatan` (`id_angkatan`, `angkatan`, `created_at`, `updated_at`) VALUES
+(1, '2020', '2021-02-02 06:50:19', NULL),
+(2, '2020', '2021-02-02 06:50:19', NULL);
 
 -- --------------------------------------------------------
 
@@ -64,6 +72,42 @@ CREATE TABLE `jurusan` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `jurusan`
+--
+
+INSERT INTO `jurusan` (`id_jurusan`, `nama_jurusan`, `singkatan_jurusan`, `created_at`, `updated_at`) VALUES
+(1, 'Teknologi Informasi', 'TI', '2021-02-02 06:50:49', NULL),
+(2, 'Akutansi', 'AK', '2021-02-10 15:26:37', NULL),
+(3, 'Teknik Elektro', 'TE', '2021-02-10 09:25:48', '2021-02-10 09:25:48'),
+(4, 'Teknik Kimia', 'TK', '2021-02-10 10:06:04', '2021-02-10 10:06:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id_kegiatan` int(10) UNSIGNED NOT NULL,
+  `nama_kegiatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_kegiatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal_kegiatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jam_kegiatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `minggu_kegiatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `detail_kegiatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kegiatan`
+--
+
+INSERT INTO `kegiatan` (`id_kegiatan`, `nama_kegiatan`, `jenis_kegiatan`, `tanggal_kegiatan`, `jam_kegiatan`, `minggu_kegiatan`, `detail_kegiatan`, `created_at`, `updated_at`) VALUES
+(1, 'Opening Mentoring 2021/2022', 'Opening Mentoring', '2021-02-06', '11.30 AM', 'Minggu 1', 'Kegiatan Opening Mentoring Bagi Mahasiswa Muslim Polinema Angkatan 2021. Online di Channel Official Youtube Rohis Polinema', '2021-02-03 05:46:31', NULL),
+(12, 'Pertemuan Zoom', 'Materi', '02/15/2021', '09:30 AM', 'Minggu 2', 'Ashiap', '2021-02-14 19:32:33', '2021-02-14 19:32:33');
+
 -- --------------------------------------------------------
 
 --
@@ -73,8 +117,8 @@ CREATE TABLE `jurusan` (
 CREATE TABLE `kelas` (
   `id_kelas` int(10) UNSIGNED NOT NULL,
   `kelas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prodi_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `jurusan_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prodi_id` int(10) NOT NULL,
+  `jurusan_id` int(10) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -134,366 +178,21 @@ CREATE TABLE `mahasiswa` (
 CREATE TABLE `materi` (
   `id_materi` int(10) UNSIGNED NOT NULL,
   `nama_materi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `minggu_materi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `link_materi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `detail_materi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mentee`
---
-
-CREATE TABLE `mentee` (
-  `id_mentee` int(10) UNSIGNED NOT NULL,
-  `nim_mentee` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nama_mentee` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kelas_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kelompok_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `angkatan_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status_mentee` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mentor`
---
-
-CREATE TABLE `mentor` (
-  `id_mentor` int(10) UNSIGNED NOT NULL,
-  `nama_mentor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_mentor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat_mentor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `notelp_mentor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `mentor`
---
-
-INSERT INTO `mentor` (`id_mentor`, `nama_mentor`, `email_mentor`, `alamat_mentor`, `notelp_mentor`, `created_at`, `updated_at`) VALUES
-(2, 'A. Yahya Hudan Permana', 'yahyahudan@mentoringpolinema.my.id', 'Jalan A. Satsui Tubun III No.83M RT 12 RW03 Malang', '081259224380', NULL, NULL),
-(3, 'Rijalus Sholihin', 'rijaluss@mentoringpolinema.my.id', 'Jalan Sepanjang Kenangan Indah No.69 Kab. Probolinggo', '082337105933', '2021-01-28 05:30:35', '2021-01-28 05:30:35'),
-(10, 'Sena Adi Fakhruddiin', 'senadi@mentoringpolinema.my.id', 'Sengkaleng City', '0812359224380', '2021-01-28 06:37:56', '2021-01-28 06:37:56');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2021_01_28_024723_create_mahasiswa_table', 1),
-(5, '2021_01_28_114810_create_mentor_table', 2),
-(6, '2021_02_02_044120_create_jurusan_table', 3),
-(7, '2021_02_02_044300_create_prodi_table', 3),
-(8, '2021_02_02_044321_create_angkatan_table', 3),
-(9, '2021_02_02_045206_create_kelompok_table', 3),
-(10, '2021_02_02_045351_create_materi_table', 3),
-(11, '2021_02_02_050625_create_pengganti_table', 3),
-(12, '2021_02_02_050756_create_mentee_table', 3),
-(13, '2021_02_02_050825_create_panitia_table', 4),
-(14, '2021_02_02_050848_create_keluhan_table', 4),
-(15, '2021_02_02_053434_create_kelas_table', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `panitia`
---
-
-CREATE TABLE `panitia` (
-  `id_panitia` int(10) UNSIGNED NOT NULL,
-  `nama_panitia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username_panitia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password_panitia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_panitia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pengganti`
---
-
-CREATE TABLE `pengganti` (
-  `id_pengganti` int(10) UNSIGNED NOT NULL,
-  `id_mentee` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file_pengganti` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `prodi`
---
-
-CREATE TABLE `prodi` (
-  `id_prodi` int(10) UNSIGNED NOT NULL,
-  `nama_prodi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `singkatan_prodi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `role`, `username`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Super Admin', 'Ahmad Yahya', 'superadmin@mentoringpolinema.my.id', NULL, '$2y$10$5VOyqn9iQuxiUAQXOoCk4OdAy2w01k8451VGKwnFsUYyxE2vqkc5O', 'XLE58WSX7Crucbrfe2L7hFYOOii4quxSeUTysfEBktvALm3xnLHjAPn0Rb3n', '2021-01-27 20:10:08', '2021-01-27 20:10:08');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `angkatan`
---
-ALTER TABLE `angkatan`
-  ADD PRIMARY KEY (`id_angkatan`);
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `jurusan`
---
-ALTER TABLE `jurusan`
-  ADD PRIMARY KEY (`id_jurusan`);
-
---
--- Indexes for table `kelas`
---
-ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_kelas`);
-
---
--- Indexes for table `kelompok`
---
-ALTER TABLE `kelompok`
-  ADD PRIMARY KEY (`id_kelompok`);
-
---
--- Indexes for table `keluhan`
---
-ALTER TABLE `keluhan`
-  ADD PRIMARY KEY (`id_keluhan`);
-
---
--- Indexes for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `materi`
---
-ALTER TABLE `materi`
-  ADD PRIMARY KEY (`id_materi`);
-
---
--- Indexes for table `mentee`
---
-ALTER TABLE `mentee`
-  ADD PRIMARY KEY (`id_mentee`);
-
---
--- Indexes for table `mentor`
---
-ALTER TABLE `mentor`
-  ADD PRIMARY KEY (`id_mentor`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `panitia`
---
-ALTER TABLE `panitia`
-  ADD PRIMARY KEY (`id_panitia`),
-  ADD UNIQUE KEY `panitia_email_panitia_unique` (`email_panitia`);
-
---
--- Indexes for table `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
-
---
--- Indexes for table `pengganti`
---
-ALTER TABLE `pengganti`
-  ADD PRIMARY KEY (`id_pengganti`);
-
---
--- Indexes for table `prodi`
---
-ALTER TABLE `prodi`
-  ADD PRIMARY KEY (`id_prodi`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `angkatan`
---
-ALTER TABLE `angkatan`
-  MODIFY `id_angkatan` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jurusan`
---
-ALTER TABLE `jurusan`
-  MODIFY `id_jurusan` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kelas`
---
-ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kelompok`
---
-ALTER TABLE `kelompok`
-  MODIFY `id_kelompok` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `keluhan`
---
-ALTER TABLE `keluhan`
-  MODIFY `id_keluhan` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `materi`
---
-ALTER TABLE `materi`
-  MODIFY `id_materi` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `mentee`
---
-ALTER TABLE `mentee`
-  MODIFY `id_mentee` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `mentor`
---
-ALTER TABLE `mentor`
-  MODIFY `id_mentor` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `panitia`
---
-ALTER TABLE `panitia`
-  MODIFY `id_panitia` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pengganti`
---
-ALTER TABLE `pengganti`
-  MODIFY `id_pengganti` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `prodi`
---
-ALTER TABLE `prodi`
-  MODIFY `id_prodi` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Error reading data for table mentoring20.materi: #2006 - MySQL server has gone away
+<div class="error"><h1>Error</h1><p><strong>SQL query:</strong>  <a href="#" class="copyQueryBtn" data-text="SET SQL_QUOTE_SHOW_CREATE = 1">Copy</a>
+<a href="db_sql.php?sql_query=SET+SQL_QUOTE_SHOW_CREATE+%3D+1&amp;show_query=1&amp;db=mentoring20"><span class="nowrap"><img src="themes/dot.gif" title="Edit" alt="Edit" class="icon ic_b_edit">&nbsp;Edit</span></a>    </p>
+<p>
+<code class="sql"><pre>
+SET SQL_QUOTE_SHOW_CREATE = 1
+</pre></code>
+</p>
+<p>
+    <strong>MySQL said: </strong><a href="./url.php?url=https%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F5.7%2Fen%2Fserver-error-reference.html" target="mysql_doc"><img src="themes/dot.gif" title="Documentation" alt="Documentation" class="icon ic_b_help"></a>
+</p>
+<code>#2006 - MySQL server has gone away</code><br></div>
