a3b9bf575ea4df040aae713296c6b861b2089195
A Yahya Hudan Permana <yahyahudan19@gmail.com>
2021-03-09 16:22:28 +0700

    rolling-back before role fixed

---
+++ resources/views/admin/mentor/index.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.master')
+@section('content')
+<div class="nk-content ">
+                    <div class="container-fluid">
+                        <div class="nk-content-inner">
+                            <div class="nk-content-body">
+                                <div class="nk-block-head nk-block-head-sm">
+                                    <div class="nk-block-between">
+                                        <div class="nk-block-head-content">
+                                            <h3 class="nk-block-title page-title">Dashboard</h3>
+                                            <h5>Selamat Datang ! {{auth()->user()->username}}</h5>
+                                        </div><!-- .nk-block-head-content -->
+                                        <div class="nk-block-head-content">
+                                            <div class="toggle-wrap nk-block-tools-toggle">
+                                                <a href="#" class="btn btn-icon btn-trigger toggle-expand mr-n1" data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
+                                                <div class="toggle-expand-content" data-content="pageMenu">
+                                                    <ul class="nk-block-tools g-3">
+                                                        <li class="nk-block-tools-opt"><a href="#" class="btn btn-primary"><em class="icon ni ni-reports"></em><span>Reports</span></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div><!-- .nk-block-head-content -->
+                                    </div><!-- .nk-block-between -->
+                                </div><!-- .nk-block-head -->
+                                <div class="nk-block">
+                                    <div class="row g-gs">
+                                        <div class="col-xxl-3 col-md-6">
+                                            <div class="card h-100">
+                                                <div class="card-inner">
+                                                    <div class="card-title-group mb-2">
+                                                        <div class="card-title">
+                                                            <h6 class="title">Data Mentor</h6>
+                                                        </div>
+                                                    </div>
+                                                    <ul class="nk-store-statistics">
+                                                        <li class="item">
+                                                            <div class="info">
+                                                                <div class="title">Data Mentee</div>
+                                                                <div class="count">3,795</div>
+                                                            </div>
+                                                            <em class="icon bg-primary-dim ni ni-users"></em>
+                                                        </li>
+                                                        <li class="item">
+                                                            <div class="info">
+                                                                <div class="title">Data Kelompok</div>
+                                                                <div class="count">120</div>
+                                                            </div>
+                                                            <em class="icon bg-info-dim ni ni-user"></em>
+                                                        </li>
+                                                        <li class="item">
+                                                            <div class="info">
+                                                                <div class="title">Data Materi</div>
+                                                                <div class="count">5</div>
+                                                            </div>
+                                                            <em class="icon bg-pink-dim ni ni-book"></em>
+                                                        </li>
+                                                    </ul>
+                                                </div><!-- .card-inner -->
+                                            </div><!-- .card -->
+                                        </div><!-- .col -->
+                                        <div class="col-lg-6 col-xxl-4">
+                                            <div class="card h-100">
+                                                <div class="card-inner border-bottom">
+                                                    <div class="card-title-group">
+                                                        <div class="card-title">
+                                                            <h6 class="title">Timeline Mentoring <?php echo date("Y");?></h6>
+                                                        </div>                                                       
+                                                    </div>
+                                                </div>
+                                                <div class="card-inner">
+                                                    <div class="timeline">
+                                                        <ul class="timeline-list">
+                                                            @foreach ($data_kegiatan as $kegiatan)
+                                                            <li class="timeline-item">
+                                                                <div class="timeline-status bg-success is-outline"></div>
+                                                                <div class="timeline-date">{{$kegiatan->tanggal_kegiatan->format('d M Y')}}</div>
+                                                                <div class="timeline-data">
+                                                                    <h6 class="timeline-title">{{$kegiatan->nama_kegiatan}}</h6>
+                                                                    <div class="timeline-des">
+                                                                        <p>{{$kegiatan->detail_kegiatan}}</p>
+                                                                        <strong><span class="time">Waktu : {{$kegiatan->jam_kegiatan}}</span></strong>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                            {{-- <li class="timeline-item">
+                                                                <div class="timeline-status bg-primary"></div>
+                                                                <div class="timeline-date">06 Jan <em class="icon ni ni-alarm-alt"></em></div>
+                                                                <div class="timeline-data">
+                                                                    <h6 class="timeline-title">Penyampaian Materi : Minggu 2</h6>
+                                                                    <div class="timeline-des">
+                                                                        <p>Materi berupa video dan Mengerjakan Kuis.</p>
+                                                                        <span class="time">09:30am</span>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                            <li class="timeline-item">
+                                                                <div class="timeline-status bg-pink"></div>
+                                                                <div class="timeline-date">07 Jan <em class="icon ni ni-alarm-alt"></em></div>
+                                                                <div class="timeline-data">
+                                                                    <h6 class="timeline-title">Pertemuan Zoom : Minggu 3</h6>
+                                                                    <div class="timeline-des">
+                                                                        <p>Pertemuan Mentee dengan Mentor melalui Media Daring Zoom.</p>
+                                                                        <span class="time">09:30am</span>
+                                                                    </div>
+                                                                </div>
+                                                            </li> --}}
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div><!-- .card -->
+                                        </div><!-- .col -->
+                                    </div><!-- .row -->
+                                </div><!-- .nk-block -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+@stop
---
+++ resources/views/admin/mentor/profile.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.master')
+@section('content')
+<div class="nk-content ">
+                    <div class="container-fluid">
+                        <div class="nk-content-inner">
+                            <div class="nk-content-body">
+                                <div class="nk-block">
+                                    <div class="card">
+                                        <div class="card-aside-wrap">
+                                            <div class="card-inner card-inner-lg">
+                                                <div class="nk-block-head nk-block-head-lg">
+                                                    <div class="nk-block-between">
+                                                        <div class="nk-block-head-content">
+                                                            <h4 class="nk-block-title">Profile Mentor</h4>
+                                                            <div class="nk-block-des">
+                                                                {{-- <p>Basic info, like your name and address, that you use on Nio Platform.</p> --}}
+                                                            </div>
+                                                        </div>
+                                                        <div class="nk-block-head-content align-self-start d-lg-none">
+                                                            <a href="#" class="toggle btn btn-icon btn-trigger mt-n1" data-target="userAside"><em class="icon ni ni-menu-alt-r"></em></a>
+                                                        </div>
+                                                    </div>
+                                                </div><!-- .nk-block-head -->
+                                                <div class="nk-block">
+                                                    <div class="nk-data data-list">
+                                                        <div class="data-head">
+                                                            <h6 class="overline-title">Basics</h6>
+                                                        </div>
+                                                        <div class="data-item" data-toggle="modal" data-target="#profile-edit">
+                                                            <div class="data-col">
+                                                                <span class="data-label">Nama Lengkap</span>
+                                                                <span class="data-value">{{auth()->user()->username}}</span>
+                                                            </div>
+                                                            <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                                        </div><!-- data-item -->
+                                                        <div class="data-item">
+                                                            <div class="data-col">
+                                                                <span class="data-label">Email</span>
+                                                                <span class="data-value">{{auth()->user()->email}}</span>
+                                                            </div>
+                                                            <div class="data-col data-col-end"><span class="data-more disable"><em class="icon ni ni-lock-alt"></em></span></div>
+                                                        </div><!-- data-item -->
+                                                        <div class="data-item" data-toggle="modal" data-target="#profile-edit">
+                                                            <div class="data-col">
+                                                                <span class="data-label">No. Telpon</span>
+                                                                <span class="data-value">+62 812 5922 4380</span>
+                                                            </div>
+                                                            <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                                        </div><!-- data-item -->
+                                                        <div class="data-item" data-toggle="modal" data-target="#profile-edit" data-tab-target="#address">
+                                                            <div class="data-col">
+                                                                <span class="data-label">Alamat</span>
+                                                                <span class="data-value">Jalan A. Satsui Tubun III No.83 M RT 12 RW 03 Malang</span>
+                                                            </div>
+                                                            <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                                        </div><!-- data-item -->
+                                                    </div><!-- data-list -->
+                                                </div><!-- .nk-block -->
+                                            </div>
+                                        </div>
+                                    </div><!-- .card -->
+                                </div><!-- .nk-block -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    <!-- @@ Profile Edit Modal @e -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="profile-edit">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <a href="#" class="close" data-dismiss="modal"><em class="icon ni ni-cross-sm"></em></a>
+                <div class="modal-body modal-body-lg">
+                    <h5 class="title">Update Profile</h5>
+                    <ul class="nk-nav nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#personal">Personal</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#address">Address</a>
+                        </li>
+                    </ul><!-- .nav-tabs -->
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="personal">
+                            <div class="row gy-4">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="form-label" for="full-name">Nama Lengkap</label>
+                                        <input type="text" class="form-control form-control-lg" id="full-name" value="Abu Bin Ishtiyak" placeholder="Enter Full name">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="form-label" for="phone-no">No. Telp</label>
+                                        <input type="text" class="form-control form-control-lg" id="phone-no" value="+880" placeholder="Phone Number">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <ul class="align-center flex-wrap flex-sm-nowrap gx-4 gy-2">
+                                        <li>
+                                            <a href="#" class="btn btn-lg btn-primary">Update Profile</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" data-dismiss="modal" class="link link-light">Cancel</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- .tab-pane -->
+                        <div class="tab-pane" id="address">
+                            <div class="row gy-4">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="form-label" for="address-l1">Alamat</label>
+                                        <input type="text" class="form-control form-control-lg" id="address-l1" value="2337 Kildeer Drive">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <ul class="align-center flex-wrap flex-sm-nowrap gx-4 gy-2">
+                                        <li>
+                                            <a href="#" class="btn btn-lg btn-primary">Update Alamat</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" data-dismiss="modal" class="link link-light">Cancel</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- .tab-pane -->
+                    </div><!-- .tab-content -->
+                </div><!-- .modal-body -->
+            </div><!-- .modal-content -->
+        </div><!-- .modal-dialog -->
+    </div><!-- .modal -->
+@stop
