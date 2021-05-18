156395d6381428eea389bad3394dadbd200e1be1
A Yahya Hudan Permana <yahyahudan19@gmail.com>
2021-05-18 07:44:53 +0700

    added tawkto.js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -98,6 +98,20 @@
     })(NioApp, jQuery);
     </script>
     @yield('jsAdd')
+
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/60a304b1b1d5182476b9cbd3/1f5ucklnn';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script>
+        <!--End of Tawk.to Script-->
     
 </body>
 
